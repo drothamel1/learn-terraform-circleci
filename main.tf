78777cd01c415b833e513f06f5cf50c3f0f4b2b9
Daniel Rothamel <daniel.rothamel@gmail.com>
2021-10-06 11:49:59 -0400

    configured backend

--- main.tf
+++ main.tf
@@ -6,6 +6,12 @@ terraform {
     }
   }
   required_version = "> 0.14"
+  
+  backend "s3" {
+  bucket = "circle-ci-backend-20211006153927459000000001"
+  key    = "terraform/webapp/terraform.tfstate"
+  region = "us-east-1"
+ }
 }
 
 backend "s3" {
